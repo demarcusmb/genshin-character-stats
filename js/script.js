248a869b581c344107b0417a3a2f3c3703703564
DeMarcus <demarcus.m.brown@gmail.com>
2022-08-19 22:24:43 -0400

    added connectors for api to html

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-const url = https://genshinlist.com/api
-const $character = $('#two')
+const url = "https://api.genshin.dev/characters"
+const $name = $('#name')
+const $description = $('#description')
+const $vision = $('#vision')
+const $weapon = $('#weapon')
+const $rarity = $('#rarity')
+const $waysObtained = $('#wayObtained')
+const $form = $('form')
+const $input = $( 'input[type="text"]' )
+function handleGetData (event)
+{
+event.preventDefault()
+userInput = $input.val()
+if ( userInput === '') return;
+$.ajax(url + userInput).then(
+    function(data) {
+        $name.text = (data.characters)
+        $input.val('')
+    }
+);
+}
