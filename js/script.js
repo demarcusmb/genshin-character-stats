e35755119593f3c95529dfd0162d2a698f461af0
DeMarcus <demarcus.m.brown@gmail.com>
2022-08-22 10:21:07 -0400

    fixed issue & added content

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
-const url = "https://api.genshin.dev/characters"
+const URL = "https://api.genshin.dev/characters/"
 const $name = $('#name')
 const $description = $('#description')
 const $vision = $('#vision')
 const $weapon = $('#weapon')
 const $rarity = $('#rarity')
-const $waysObtained = $('#wayObtained')
+const $constellation = $('#constellation')
+const $faction = $('#faction')
+ const $nation = $('#nation')
 const $form = $('form')
 const $input = $( 'input[type="text"]' )
+$form.on( 'submit', handleGetData )
 function handleGetData (event)
 {
 event.preventDefault()
 userInput = $input.val()
 if ( userInput === '') return;
-$.ajax(url + userInput).then(
-    function(data) {
-        $name.text = (data.characters)
+$.ajax(URL+userInput).then(
+     function (data) {
+        console.log(data)
+         $name.text  (data.name)
+        $description.text  (data.description)
+        $vision.text  (data.vision)
+        $weapon.text (data.weapon)
+        $rarity.text  (data.rarity)
+        $constellation.text (data.constellation)
+        $faction.text (data.affiliation)
+        $nation.text (data.nation)
         $input.val('')
     }
-);
-}
+ );
+} 
